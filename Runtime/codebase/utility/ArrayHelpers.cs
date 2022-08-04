643a662919dd957966fda78a3df8e538aeeeaa13
Gabriele Picco <piccogabriele@gmail.com>
2022-08-04 20:20:31 +0200

    :recycle: Code refactor
    Code refactor & Improvement

--- Runtime/codebase/utility/ArrayHelpers.cs
+++ Runtime/codebase/utility/ArrayHelpers.cs
@@ -14,10 +14,9 @@ namespace Merkator.Tools
 			Contract.Ensures(Contract.Result<T[]>().Length == arrays.Sum(arr => arr.Length));
 
 			var result = new T[arrays.Sum(arr => arr.Length)];
-			int offset = 0;
-			for (int i = 0; i < arrays.Length; i++)
+			var offset = 0;
+			foreach (var arr in arrays)
 			{
-				var arr = arrays[i];
 				Buffer.BlockCopy(arr, 0, result, offset, arr.Length);
 				offset += arr.Length;
 			}
