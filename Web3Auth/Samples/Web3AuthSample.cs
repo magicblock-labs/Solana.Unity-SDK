d43481ae86eedb458641b4e8f4eaef64d2854685
AkselPaksel <109410577+AkselPaksel@users.noreply.github.com>
2022-08-21 16:32:23 +0200

    Add files via upload (#14)
    Initial Web3Auth

---
+++ Web3Auth/Samples/Web3AuthSample.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class Web3AuthSample : MonoBehaviour
+{
+    List<LoginVerifier> verifierList = new List<LoginVerifier> {
+        new LoginVerifier("Google", Provider.GOOGLE),
+        new LoginVerifier("Facebook", Provider.FACEBOOK),
+        new LoginVerifier("CUSTOM_VERIFIER", Provider.CUSTOM_VERIFIER),
+        new LoginVerifier("Twitch", Provider.TWITCH),
+        new LoginVerifier("Discord", Provider.DISCORD),
+        new LoginVerifier("Reddit", Provider.REDDIT),
+        new LoginVerifier("Apple", Provider.APPLE),
+        new LoginVerifier("Github", Provider.GITHUB),
+        new LoginVerifier("LinkedIn", Provider.LINKEDIN),
+        new LoginVerifier("Twitter", Provider.TWITTER),
+        new LoginVerifier("Line", Provider.LINE),
+        new LoginVerifier("Hosted Email Passwordless", Provider.EMAIL_PASSWORDLESS),
+    };
+
+    Web3Auth web3Auth;
+
+
+    [SerializeField]
+    Text loginResponseText;
+
+    public void Start()
+    {
+        var loginConfigItem = new LoginConfigItem()
+        {
+            verifier = "your_verifierid_from_web3auth_dashboard",
+            typeOfLogin = TypeOfLogin.GOOGLE,
+            clientId = "BOKX2WS_5WVzZNu_NUCB141y6KLTPzHqC8A2M-Az0nlw6yW54pS4J8uHs5BVtVG4iDIiQSf2keAN9XPc_MA1Pq4"
+        };
+
+    }
+
+    public void onLogin(Web3AuthResponse response)
+    {
+        loginResponseText.text = JsonConvert.SerializeObject(response, Formatting.Indented);
+        var userInfo = JsonConvert.SerializeObject(response.userInfo, Formatting.Indented);
+    }
+
+    public void login(Web3Auth web3Auth)
+    {
+        //var selectedProvider = verifierList[verifierDropdown.value].loginProvider;
+        var selectedProvider = Provider.GOOGLE;
+
+
+        var options = new LoginParams()
+        {
+            loginProvider = selectedProvider
+        };
+
+        web3Auth.login(options);
+    }
+}
---
+++ Web3Auth/Utils.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class Utils
+{
+    //https://qiita.com/lucifuges/items/b17d602417a9a249689f
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    extern static void launchUrl(string url);
+    [DllImport("__Internal")]
+    extern static void dismiss();
+#endif
+
+
+    public static void LaunchUrl(string url)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        Application.OpenURL(url);
+#elif UNITY_ANDROID
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var browserView = new AndroidJavaObject("com.web3auth.unity.android.BrowserView"))
+        {
+            browserView.CallStatic("launchUrl", activity, url);
+        }
+
+#elif UNITY_IOS
+    launchUrl(url);;
+#endif
+    }
+
+    public static void Dismiss()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+    dismiss();
+#endif
+    }
+
+
+    public static byte[] DecodeBase64(string text)
+    {
+        var output = text;
+        output = output.Replace('-', '+');
+        output = output.Replace('_', '/');
+        switch (output.Length % 4)
+        {
+            case 0: break;
+            case 2: output += "=="; break;
+            case 3: output += "="; break;
+            default: throw new FormatException(text);
+        }
+        var converted = Convert.FromBase64String(output);
+        return converted;
+    }
+
+    public static Dictionary<string, string> ParseQuery(string text)
+    {
+        if (text.Length > 0 && text[0] == '?')
+            text = text.Remove(0, 1);
+
+        var parts = text.Split('&').Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+        Dictionary<string, string> result = new Dictionary<string, string>();
+
+        if (parts.Count > 0)
+        {
+            result = parts.ToDictionary(
+                c => c.Split('=')[0],
+                c => Uri.UnescapeDataString(c.Split('=')[1])
+            );
+        }
+
+        return result;
+    }
+
+    public static int GetRandomUnusedPort()
+    {
+        var listener = new TcpListener(IPAddress.Any, 0);
+        listener.Start();
+        var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+        listener.Stop();
+        return port;
+    }
+
+
+}
