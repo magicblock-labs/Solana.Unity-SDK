fa7eb7cb0b95d14b1d52a847e9a70aa39752943b
Gabriele Picco <piccogabriele@gmail.com>
2023-02-01 21:43:35 +0300

    :bug: Add missing linked functions

--- Runtime/Plugins/NativeWebSocket/iOS/NativeWebSocket.mm
+++ Runtime/Plugins/NativeWebSocket/iOS/NativeWebSocket.mm
@@ -4,13 +4,23 @@ extern "C"
 
   int WebSocketAddSubProtocol (int instanceId, const char * subprotocol){}
 
-  void WebSocketFree (int instanceId);
+  void WebSocketFree (int instanceId){}
 
-  void WebSocketSetOnOpen (void * callback);
+  void WebSocketSetOnOpen (void * callback){}
 
-  void WebSocketSetOnMessage (void * callback);
+  void WebSocketSetOnMessage (void * callback){}
 
-  void WebSocketSetOnError (void * callback);
+  void WebSocketSetOnError (void * callback){}
 
-  void WebSocketSetOnClose (void * callback);
+  void WebSocketSetOnClose (void * callback){}
+
+  int WebSocketConnect (int instanceId){}
+
+  int WebSocketClose (int instanceId, int code, char * reason){}
+
+  int WebSocketSend (int instanceId, void * dataPtr, int dataLength){}
+
+  int WebSocketSendText (int instanceId, char * message){}
+
+  int WebSocketGetState (int instanceId){}
 }
