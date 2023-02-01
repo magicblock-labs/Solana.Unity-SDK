6c10abb85613edd148843e970162e5059908bc1b
Gabriele Picco <piccogabriele@gmail.com>
2023-02-01 21:22:47 +0300

    :heavy_plus_sign: Add mapped functions

--- Runtime/Plugins/NativeWebSocket/iOS/NativeWebSocket.mm
+++ Runtime/Plugins/NativeWebSocket/iOS/NativeWebSocket.mm
@@ -1,4 +1,18 @@
 extern "C"
 {
   void WebSocketAllocate(const char * url){}
+
+  void WebSocketAllocate(const char * url){}
+
+  int WebSocketAddSubProtocol (int instanceId, const char * subprotocol){}
+
+  void WebSocketFree (int instanceId);
+
+  void WebSocketSetOnOpen (void * callback);
+
+  void WebSocketSetOnMessage (void * callback);
+
+  void WebSocketSetOnError (void * callback);
+
+  void WebSocketSetOnClose (void * callback);
 }
