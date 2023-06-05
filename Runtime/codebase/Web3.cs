c4d31cd509a1c893c0439508535426e53cbcb4d6
Gabriele Picco <piccogabriele@gmail.com>
2023-06-05 14:11:48 +0200

    :sparkles: Improve Nfts loading and add option to only load metadata

--- Runtime/codebase/Web3.cs
+++ Runtime/codebase/Web3.cs
@@ -98,6 +98,8 @@ namespace Solana.Unity.SDK
         }
         
         private static List<Nft.Nft> _nfts = new();
+        private static bool _isLoadingNfts = false;
+
         public delegate void NFTsUpdate(List<Nft.Nft> nfts, int total);
         private static event NFTsUpdate OnNFTsUpdateInternal;
         public static event NFTsUpdate OnNFTsUpdate
@@ -292,7 +294,7 @@ namespace Solana.Unity.SDK
             _solAmount = balance;
             OnBalanceChangeInternal?.Invoke(balance);
         }
-        
+
         /// <summary>
         /// Update the list of NFTs owned by the current wallet
         /// Notify all registered listeners
@@ -300,11 +302,29 @@ namespace Solana.Unity.SDK
         /// <param name="commitment"></param>
         public static async UniTask UpdateNFTs(Commitment commitment = Commitment.Confirmed)
         {
-            if(Wallet == null) return;
+            if(_isLoadingNfts) return;
+            _isLoadingNfts = true;
+            await LoadNFTs(notifyRegisteredListeners: true, commitment: commitment);
+            _isLoadingNfts = false;
+        }
+
+        /// <summary>
+        /// Update the list of NFTs owned by the current wallet
+        /// Notify all registered listeners
+        /// </summary>
+        /// <param name="loadTexture"></param>
+        /// <param name="notifyRegisteredListeners">If true, notify the register listeners</param>
+        /// <param name="commitment"></param>
+        public static async UniTask<List<Nft.Nft>> LoadNFTs(
+            bool loadTexture = true, 
+            bool notifyRegisteredListeners = false,
+            Commitment commitment = Commitment.Confirmed)
+        {
+            if(Wallet == null) return null;
             var tokens = (await Wallet.GetTokenAccounts(commitment))?
                 .ToList()
                 .FindAll(m => m.Account.Data.Parsed.Info.TokenAmount.AmountUlong == 1);
-            if(tokens == null) return;
+            if(tokens == null) return null;
             
             // Remove tokens not owned anymore
             var tkToRemove = new List<Nft.Nft>();
@@ -327,23 +347,30 @@ namespace Solana.Unity.SDK
                 .FindAll(x => x.metaplexData.data.offchainData != null);
 
             // Fetch nfts
+            List<UniTask> loadingTasks = new List<UniTask>();
+            List<Nft.Nft> nfts = new List<Nft.Nft>(_nfts);
             if (tokens is {Count: > 0})
             {
                 var toFetch = tokens
                     .Where(item => item.Account.Data.Parsed.Info.TokenAmount.AmountUlong == 1)
-                    .Where(item => _nfts
-                        .All(t => t.metaplexData.data.mint!= item.Account.Data.Parsed.Info.Mint));
+                    .Where(item => nfts
+                        .All(t => t.metaplexData.data.mint!= item.Account.Data.Parsed.Info.Mint)).ToArray();
                 foreach (var item in toFetch)
                 {
-                    Nft.Nft.TryGetNftData(item.Account.Data.Parsed.Info.Mint, Rpc).AsUniTask()
-                        .ContinueWith(nft =>
+                    var tNft = Nft.Nft.TryGetNftData(item.Account.Data.Parsed.Info.Mint, Rpc, loadTexture: loadTexture).AsUniTask();
+                    loadingTasks.Add(tNft);
+                    tNft.ContinueWith(nft =>
                         {
                             if(nft == null) return;
-                            _nfts.Add(nft);
-                            OnNFTsUpdateInternal?.Invoke(_nfts, _nfts.Count + toFetch.Count());
+                            nfts.Add(nft);
+                            if(notifyRegisteredListeners) 
+                                OnNFTsUpdateInternal?.Invoke(nfts, nfts.Count + toFetch.Length);
                         }).Forget();
                 }
             }
+            await UniTask.WhenAll(loadingTasks);
+            _nfts = nfts;
+            return nfts;
         }
         
         private static async UniTask SubscribeToWalletEvents(Commitment commitment = Commitment.Confirmed)
@@ -357,7 +384,7 @@ namespace Solana.Unity.SDK
                     Debug.Log("Account changed!, updated lamport: " + accountInfo.Value.Lamports);
                     _solAmount = accountInfo.Value.Lamports / 1000000000d;
                     OnBalanceChangeInternal?.Invoke(_solAmount);
-                    UpdateNFTs(commitment).Forget();
+                    UpdateNFTs(commitment: commitment).Forget();
                 },
                 commitment
             );
