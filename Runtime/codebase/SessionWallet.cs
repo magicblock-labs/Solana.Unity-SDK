72a8afe3708a5d6cf886b7cecc516e704fb0138f
abishekk92 <bhat.abishek@gmail.com>
2023-06-23 16:06:37 +0530

    Add convenience methods

--- Runtime/codebase/SessionWallet.cs
+++ Runtime/codebase/SessionWallet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,7 +7,11 @@ using Solana.Unity.KeyStore.Exceptions;
 using Solana.Unity.KeyStore.Services;
 using Solana.Unity.Rpc.Models;
 using Solana.Unity.Wallet;
+using Solana.Unity.Programs;
 using Solana.Unity.Wallet.Bip39;
+using Solana.Unity.Gum.GplSession;
+using Solana.Unity.Gum.GplSession.Accounts;
+using Solana.Unity.Gum.GplSession.Program;
 using UnityEngine;
 
 // ReSharper disable once CheckNamespace
@@ -17,100 +22,134 @@ namespace Solana.Unity.SDK
     {
         private const string EncryptedKeystoreKey = "SessionKeystore";
 
+        public PublicKey TargetProgram { get; protected set; }
+        public PublicKey SessionTokenPDA { get; protected set; }
+
         public SessionWallet(RpcCluster rpcCluster = RpcCluster.DevNet,
             string customRpcUri = null, string customStreamingRpcUri = null,
             bool autoConnectOnStartup = false) : base(rpcCluster, customRpcUri, customStreamingRpcUri, autoConnectOnStartup)
         {
         }
 
-        /// <summary>
-        /// Returns an instance of Keypair from a mnemonic
-        /// </summary>
-        /// <param name="mnemonic"></param>
-        /// <returns></returns>
-        private static Account FromMnemonic(string mnemonic)
+        public static bool HasSessionWallet()
         {
-            var wallet = new Wallet.Wallet(new Mnemonic(mnemonic));
-            return wallet.Account;
+            var prefs = LoadPlayerPrefs(EncryptedKeystoreKey);
+            return !string.IsNullOrEmpty(prefs);
         }
 
-        /// <summary>
-        /// Returns an instance of Keypair from a secret key
-        /// </summary>
-        /// <param name="secretKey"></param>
-        /// <returns></returns>
-        private static Account FromSecretKey(string secretKey)
+        private PublicKey FindSessionToken() {
+            return SessionToken.DeriveSessionTokenAccount(
+                authority: Web3.Account,
+                targetProgram: TargetProgram,
+                sessionSigner: Account.PublicKey
+            );
+        }
+
+        public static async Task<SessionWallet> GetSessionWallet(PublicKey targetProgram, string password, RpcCluster rpcCluster = RpcCluster.DevNet,
+            string customRpcUri = null, string customStreamingRpcUri = null,
+            bool autoConnectOnStartup = false)
         {
+            SessionWallet sessionWallet = new SessionWallet(rpcCluster, customRpcUri, customStreamingRpcUri, autoConnectOnStartup);
+            sessionWallet.TargetProgram = targetProgram;
+            if (HasSessionWallet())
+            {
+                await sessionWallet.Login(password);
+            }
+            else {
+                await sessionWallet.CreateAccount(password);
+            }
+            sessionWallet.SessionTokenPDA = sessionWallet.FindSessionToken();
+            return sessionWallet;
+        }
+
+        /// <inheritdoc />
+        protected override Task<Account> _Login(string password = "")
+        {
+            var keystoreService = new KeyStorePbkdf2Service();
+            var encryptedKeystoreJson = LoadPlayerPrefs(EncryptedKeystoreKey);
+            byte[] decryptedKeystore;
             try
             {
-                var wallet = new Wallet.Wallet(new PrivateKey(secretKey).KeyBytes, "", SeedMode.Bip39);
-                return wallet.Account;
+                if (string.IsNullOrEmpty(encryptedKeystoreJson))
+                    return Task.FromResult<Account>(null);
+                decryptedKeystore = keystoreService.DecryptKeyStoreFromJson(password, encryptedKeystoreJson);
             }
-            catch (ArgumentException)
+            catch (DecryptionException)
             {
-                return null;
+                return Task.FromResult<Account>(null);
             }
 
+            var secret = Encoding.UTF8.GetString(decryptedKeystore);
+            var account = FromSecret(secret);
+            if (IsMnemonic(secret))
+            {
+                var restoredMnemonic = new Mnemonic(secret);
+                Mnemonic = restoredMnemonic;
+            }
+            return Task.FromResult(account);
         }
 
-        /// <summary>
-        /// Returns an instance of Keypair from a Byte Array
-        /// </summary>
-        /// <param name="secretByteArray"></param>
-        /// <returns></returns>
-        private static Account FromByteArray(byte[] secretByteArray)
+        /// <inheritdoc />
+        protected override Task<Account> _CreateAccount(string secret = null, string password = null)
         {
-            var wallet = new Wallet.Wallet(secretByteArray, "", SeedMode.Bip39);
-            return wallet.Account;
+            Account account;
+            Mnemonic mnem = null;
+            if (secret != null)
+            {
+                account = FromSecret(secret);
+                if (IsMnemonic(secret))
+                {
+                    mnem = new Mnemonic(secret);
+                }
+            }
+            else
+            {
+                mnem = new Mnemonic(WordList.English, WordCount.Twelve);
+                var wallet = new Wallet.Wallet(mnem);
+                account = wallet.Account;
+                secret = mnem.ToString();
+            }
+            if(account == null) return Task.FromResult<Account>(null);
+
+            password ??= "";
+
+            var keystoreService = new KeyStorePbkdf2Service();
+            var stringByteArray = Encoding.UTF8.GetBytes(secret);
+            var encryptedKeystoreJson = keystoreService.EncryptAndGenerateKeyStoreAsJson(
+                password, stringByteArray, account.PublicKey.Key);
+
+            SavePlayerPrefs(EncryptedKeystoreKey, encryptedKeystoreJson);
+            Mnemonic = mnem;
+            return Task.FromResult(account);
         }
 
-        /// <summary>
-        /// Takes a string as input and checks if it is a valid mnemonic
-        /// </summary>
-        /// <param name="secret"></param>
-        /// <returns></returns>
-        private static bool IsMnemonic(string secret)
-        {
-            return secret.Split(' ').Length is 12 or 24;
-        }
-        /// <summary>
-        /// Takes a string as input and checks if it is a valid byte array
-        /// </summary>
-        /// <param name="secret"></param>
-        /// <returns></returns>
-        private static bool IsByteArray(string secret)
-        {
-            return secret.StartsWith('[') && secret.EndsWith(']');
+        public TransactionInstruction CreateSessionIx(bool topUp, long sessionValidity) {
+            CreateSessionAccounts createSessionAccounts = new CreateSessionAccounts()
+            {
+                SessionToken = SessionTokenPDA,
+                SessionSigner = Account.PublicKey,
+                Authority = Web3.Account,
+                TargetProgram = TargetProgram,
+                SystemProgram = SystemProgram.ProgramIdKey,
+            };
+
+            return GplSessionProgram.CreateSession(
+                createSessionAccounts,
+                topUp: topUp,
+                validUntil: sessionValidity
+            );
         }
 
-        /// <summary>
-        /// Takes a string as input and tries to parse it into a Keypair
-        /// </summary>
-        /// <param name="secret"></param>
-        /// <returns></returns>
-        private static Account ParseByteArray(string secret)
-        {
-            var parsed = secret
-                .Replace(",]", "]")
-                .Trim('[', ']')
-                .Split(',')
-                .Select(byte.Parse).ToArray();
-
-            return FromByteArray(parsed);
+        public async Task<bool> IsSessionTokenInitialized() {
+            var sessionTokenData = await ActiveRpcClient.GetAccountInfoAsync(SessionTokenPDA);
+            return sessionTokenData.Result.Value.Data[0] != null;
         }
 
-
-        private static string LoadPlayerPrefs(string key)
-        {
-            return PlayerPrefs.GetString(key);
+        public async Task<bool> IsSessionTokenValid() {
+            var sessionTokenData = (await ActiveRpcClient.GetAccountInfoAsync(SessionTokenPDA)).Result.Value.Data[0];
+            if (sessionTokenData == null) return false;
+            return SessionToken.Deserialize(Convert.FromBase64String(sessionTokenData)).ValidUntil > DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         }
 
-        private static void SavePlayerPrefs(string key, string value)
-        {
-            PlayerPrefs.SetString(key, value);
-            #if UNITY_WEBGL
-            PlayerPrefs.Save();
-            #endif
-        }
     }
 }
