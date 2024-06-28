c2b97d6e56dbecb2ef6cbd3d63ee2d51351063d6
Gabriele Picco <gabriele@magicblock.gg>
2024-06-28 12:29:42 +0100

    :bug: Add [Preserve] attribute to fix code stripping & Simplify Android manifest

--- Runtime/Plugins/Web3AuthSDK/Types/LoginParams.cs
+++ Runtime/Plugins/Web3AuthSDK/Types/LoginParams.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using UnityEngine.Scripting;
 
+[Preserve]
+[Serializable]
 public class LoginParams
 {
+    [Preserve]
     public Provider loginProvider { get; set; }
+    [Preserve]
     public string dappShare { get; set; }
+    [Preserve]
     public ExtraLoginOptions extraLoginOptions { get; set; }
+    [Preserve]
     public Uri redirectUrl { get; set; }
+    [Preserve]
     public string appState { get; set; }
+    [Preserve]
     public MFALevel mfaLevel { get; set; }
-
+    [Preserve]
     public Curve curve { get; set; } = Curve.SECP256K1;
+    [Preserve]
     public string dappUrl { get; set; }
 }
